5611d1cf0c2856dbd011b371fa6626e7f65c9992
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-26 08:26:18 +0100

    Improves UI / UX of the dashboard

--- scripts/10_cap_valuation_screener.py
+++ scripts/10_cap_valuation_screener.py
@@ -6,8 +6,9 @@ from yahoo_fin_api import YahooFinApi, Client, FileCache, Universe, Ticker
 from lib.utils import get_symbol_img, format_amount, format_percentage
 
 BILLION = 1000000000
+HOUR = 60 * 60 #in seconds
 
-@st.experimental_memo
+@st.experimental_memo(ttl=HOUR)
 def load_data(index: str)-> List[Ticker]:
 	if index == "FTSE":
 		symbols = Universe.get_ftse_100_universe()
@@ -16,7 +17,7 @@ def load_data(index: str)-> List[Ticker]:
 	else:
 		symbols = Universe.get_freetrade_universe()
 
-	yf = YahooFinApi(Client(FileCache("./data")))
+	yf = YahooFinApi(Client())
 	if symbols is None:
 		raise Exception("symbols not found")
 	return yf.get_all(symbols)
@@ -75,14 +76,18 @@ def display_stock_list(container, data: list)-> None:
 			container.image(symbol_img, width=50)
 
 		container.markdown(f"**{symbol}**")
-		container.markdown(f"**Market cap:**  {format_amount(market_cap)}")
-		container.markdown(f"**Free cash flow:**  {format_amount(fcf)}")
-		container.markdown(f"**Profit margin:**  {format_percentage(profit_margin)}")
-		container.markdown(
-			f"**Cap rate:**  <span style='color:yellow;'>{format_percentage(cap_rate)}</span>",
-			unsafe_allow_html=True
-		)
+		container.markdown(display_line("Market cap", format_amount(market_cap)), unsafe_allow_html=True)
+		container.markdown(display_line("Free cash flow", format_amount(fcf)), unsafe_allow_html=True)
+		container.markdown(display_line("Profit margin", format_percentage(profit_margin)), unsafe_allow_html=True)
+		container.markdown(display_line("Cap rate", f"<span style='color:yellow;'>{format_percentage(cap_rate)}</span>"), unsafe_allow_html=True)
+		container.markdown(f"[See more](https://uk.finance.yahoo.com/quote/{symbol})")
 
+def display_line(label: str, value: str)-> str:
+	return f"""
+	<span style='display: flex; justify-content: space-between'>
+		<p><strong>{label}:</strong></p>
+		<p>{value}</p>
+	</span>""".strip()
 
 ###########
 # Sidebar #
@@ -105,7 +110,7 @@ show_top_limit = st.sidebar.number_input(
 
 st.title("10 Cap Valuation Screener")
 
-col1, col2 = st.columns(2)
+col1, _, col2 = st.columns([2, 0.5, 2])
 
 ############
 # Column 1 #
